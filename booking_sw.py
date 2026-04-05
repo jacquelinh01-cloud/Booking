9022da0215c32e6881b6182a03b99ce1e85337b7
jacquelinh01-cloud <jacquelinh01@gmail.com>
2026-04-05 16:45:24 -0600

    Update booking_sw.py
    updated booking button and csv issues

--- booking_sw.py
+++ booking_sw.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import csv
 
 date = st.date_input("Select a date")
 
@@ -7,6 +8,13 @@ time = st.selectbox("Select time", ["9:00 am", "1:00 PM", "5:00 PM"])
 name = st.text_input("Name")
 
 people = st.number_input("Number of people", min_value=1)
+
+if st.button("Book Now"):
+    with open("bookings.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([name, date, time, people])
+
+    st.success("Booking saved!")
 
 def check_avail(date, time, new_people):
     total = 0
@@ -18,6 +26,7 @@ def check_avail(date, time, new_people):
             if row[1] == str(date) and row[2] == time:
                 total += int(row[3])
     return total +new_people <= 35
+
 
 
 
