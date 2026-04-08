f22c34bed2b6dd2acb83fa1755d69cee25052dc1
Jacquelin Horval <jacquelinh01@gmail.com>
2026-04-08 15:59:45 -0600

    initial booking app update

--- booking_sw.py
+++ booking_sw.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import streamlit as st
 import csv
 import pandas as pd
@@ -50,3 +51,62 @@ def save_booking(name, date, time, guests):
 
 
 
+=======
+import streamlit as st
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+st.title("Booking System")
+
+name = st.text_input("Name")
+email = st.text_input("Email")
+date = st.date_input("Select a date")
+time = st.selectbox("Select time", ["9:00 AM", "1:00 PM", "5:00 PM"])
+service = st.text_input("Service", ["Local", "Standard"])
+guests = st.number_input("Guests", min_value=1)
+
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+client = gspread.authorize(creds)
+
+sheet = client.open_by_key("1d7Pc_Va8rMhfPBYzjPMeKPpuYPfpRCpc9vBB1_yP_64")
+
+worksheet = sheet.worksheet("Bookings")
+
+def save_booking(name, date, time, guests):
+    worksheet.append_row([
+        name,
+        email,
+        str(date),
+        time,
+        service,
+        guests
+    ])
+
+def check_avail(date, time, new_guests):
+    total = 0
+    records = worksheet.get_all_records()
+
+    for row in records:
+        if str(row['date']) == str(date) and row['time'] == time:
+            total += int(row['guests'])
+    return total + new_guests <= 35
+
+
+
+if st.button("Book Now"):
+    if check_avail(date, time, guests):
+        save_booking(name, email, date, time, service, guests)
+        st.success("Booking saved!")
+    else:
+        st.error("Slot full")
+
+
+
+
+
+>>>>>>> b2c804f (initial booking app update)
